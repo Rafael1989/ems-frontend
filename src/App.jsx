b6159883f1967e2a60379db4eff8c5e3d0d0292f
Rafael <flns_rafa@hotmail.com>
2026-02-23 18:27:27 +0100

    Add update e remove

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import EmployeeComponent from './components/EmployeeComponent'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
           <Routes>
             <Route path='/' element = { <ListEmployeeComponent />}></Route>
             <Route path='/employees' element = { <ListEmployeeComponent />}></Route>
+            <Route path='/add-employee' element = { <EmployeeComponent />}></Route>
+            <Route path='/edit-employee/:id' element = { <EmployeeComponent />}></Route>
           </Routes>
         <FooterComponent />
       </BrowserRouter>
